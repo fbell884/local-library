67bbbe0fc4145ede32f426dc36feeca7607ace30
Francis Bell <francisbell@Franciss-MBP.fios-router.home>
2023-02-06 10:38:22 -0500

    defined locallibrary db schema & populate sample data

--- app.js
+++ app.js
@@ -9,6 +9,17 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+// Database connection: 
+const mongoose = require("mongoose");
+mongoose.set('strictQuery', false);
+const mongoDB = "mongodb+srv://fbell884:Rmsalhhsd16@cluster0.5u3did7.mongodb.net/local_library?retryWrites=true&w=majority";
+
+main().catch(err => console.log(err));
+async function main() {
+  await mongoose.connect(mongoDB);
+}
+
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
---
+++ models/book.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const BookSchema = new Schema({
+    title: {type: String, required: true},
+    author: {type: Schema.Types.ObjectId, ref: "Author", required: true},
+    summary: {type: String, required: true},
+    genre: [{type: Schema.Types.ObjectId, ref: "genre"}],
+});
+
+// Virtual for book's URL (Mongoose Docs: In Mongoose, a virtual 
+// is a property that is not stored in MongoDB. Virtuals are typically used for computed properties on documents.)
+
+BookSchema.virtual("url").get(function () {
+    return `/catalog/book/${this._id}`;
+});
+
+// Export 
+module.exports = mongoose.model("Book", BookSchema);
